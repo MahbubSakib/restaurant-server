2fc95e27068dbe0574cd0b3c162cb42ddd7190ba
mahbubsakib <sakibmahbub92@gmail.com>
2025-01-08 14:47:43 +0600

    delete, make admin an user

--- index.js
+++ index.js
@@ -31,6 +31,49 @@ async function run() {
         const menuCollection = client.db("dineFineDB").collection("menu");
         const reviewCollection = client.db("dineFineDB").collection("reviews");
         const cartCollection = client.db("dineFineDB").collection("carts");
+        const userCollection = client.db("dineFineDB").collection("users");
+
+
+        // users
+        // get users
+        app.get('/users', async (req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        })
+
+
+
+        // create an user
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email }
+            const existingUser = await userCollection.findOne(query)
+            if (existingUser) {
+                return res.send({ message: 'user already exist', insertedId: null })
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updatedDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc);
+            res.send(result);
+        })
+
+        // delete user
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        })
 
         // get all menu
         app.get('/menu', async (req, res) => {
@@ -47,9 +90,9 @@ async function run() {
         // ----- cart -----------
 
         // get carts
-        app.get('/carts', async(req,res)=>{
+        app.get('/carts', async (req, res) => {
             const email = req.query.email;
-            const query = {email: email}
+            const query = { email: email }
             const result = await cartCollection.find(query).toArray();
             res.send(result);
         })
@@ -62,9 +105,9 @@ async function run() {
         })
 
         // cart delete
-        app.delete('/carts/:id', async(req,res)=>{
+        app.delete('/carts/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await cartCollection.deleteOne(query);
             res.send(result);
         })
